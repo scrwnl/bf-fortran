fe9ed1bce899beccb065b18951afc3e39023138a
Nejikugi <63852665+scrwnl@users.noreply.github.com>
2024-05-10 00:43:46 +0900

    added main.f90

---
+++ main.f90
@@ -0,0 +1,9 @@
+program main
+    use mod_readline
+    implicit none
+    integer :: codeptr = 0
+    integer :: memptr = 0
+    character(:), allocatable :: code
+    character memory(3000)
+
+end program main
